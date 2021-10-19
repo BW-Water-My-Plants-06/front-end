1c100ab5557e22143fc1e031db3ba280f1f997ce
mspicola <mspicola@protonmail.com>
2021-10-19 16:03:13 -0400

    created listofPlants component, dummy data

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './components/listofPlants';
 
 function App() {
   return (
---
+++ water-my-plants/src/components/listofPlants.js
@@ -0,0 +1,26 @@
+const myPlants = [{
+    id: 1,
+    nickname: 'weed',
+    species: 'cannabis',
+    h2oFrequency: 2,
+    image: 'image url'
+},
+
+{
+    id: 2,
+    nickname: 'spice',
+    species: 'oregano',
+    h2oFrequency: 3,
+    image: 'image url'
+},    
+
+{   id: 3,
+    nickname: 'rose',
+    species: 'flower',
+    h2oFrequency: 4,
+    image: 'image url'
+}];
+
+function ListPlants() {
+    return <h2>List of Plants</h2>;
+  }
