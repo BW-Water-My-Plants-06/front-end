f04ada74dc60038758d920d3e007ddd1654134b2
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-10-20 13:31:38 -0400

    Fixed all changes

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -4,16 +4,17 @@ import styled from 'styled-components';
 import { AccountBox } from './components/accountBox';
 import NavBar from './components/NavBar';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import PlantList from './components/PlantList';
+import PlantDetail from './components/PlantDetail';
 //components
 
-import NavBar from ''
-import footer from ''
+// import footer from ''
 
 //Route 
-import Home from "./components/Home"
-import ResetPassword from "./components/ResetPassword"
-import DashBoard from ''
-import Contact from ''
+// import Home from "./components/Home"
+// import ResetPassword from "./components/ResetPassword"
+// import DashBoard from ''
+// import Contact from ''
 
 const AppContainer = styled.div`
   width: 100%;
@@ -30,13 +31,14 @@ function App() {
       <NavBar />
       <AppContainer>
         <Switch>
-          <Route exact path="/" />
+          {/* <Route exact path="/" /> */}
           <Route path="/signin" component={AccountBox} />
           <Route path="/signup" component={AccountBox} />
-          <Route path='/' exact component='Home'/>
+          {/* <Route path='/' exact component='Home'/>
           <Route path='/' exact component='DashBoard'/>
           <Route path='/' exact component='Contact'/>
-          <Route path='/' exact component='ResetPassword'/>
+          <Route path='/' exact component='ResetPassword'/> */}
+          <PlantList />
         </Switch>
       </AppContainer> 
     </BrowserRouter>
