eb57e09e1a4d89b76d693e884f7e5f5a6b10e6a3
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-10-20 12:37:47 -0400

    made some changes

---
+++ water-my-plants/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import PlantList from './components/PlantList';
+import PlantDetail from './components/PlantDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <PlantList />
+    </div>
+  );
+}
+
+export default App;
+
+
+// testing
---
+++ water-my-plants/src/components/PlantList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import PlantDetail from './components/PlantDetail';
+
+const myPlants = [{
+    id: 1,
+    nickname: 'weed',
+    species: 'cannabis',
+    h2oFrequency: 2,
+    image: 'image url'
+},
+
+{
+    id: 2,
+    nickname: 'spice',
+    species: 'oregano',
+    h2oFrequency: 3,
+    image: 'image url'
+},    
+
+{   id: 3,
+    nickname: 'rose',
+    species: 'flower',
+    h2oFrequency: 4,
+    image: 'image url'
+}];
+
+function PlantList() {
+    return <h2>List of Plants</h2>;
+  }
+
+  export default PlantList;
